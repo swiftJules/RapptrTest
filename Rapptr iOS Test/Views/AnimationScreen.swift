379beebd6cd2dbe24217ccdbd8d9091800d9e561
swiftJules <juliana.connors.dev@gmail.com>
2022-03-15 14:01:08 -0400

    add properties and logic to fade logo in and out on button tap

--- Rapptr iOS Test/Views/AnimationScreen.swift
+++ Rapptr iOS Test/Views/AnimationScreen.swift
@@ -8,16 +8,31 @@
 import SwiftUI
 
 struct AnimationScreen: View {
+    @State var animationShown: Bool = false
+    @State var fadeIn: Bool = false
     var body: some View {
         VStack {
-            // display animation
             Spacer()
-            Image("ic_logo")
+            if fadeIn {
+                Image("ic_logo")
+                    .resizable()
+                    .scaledToFit()
+            }
             Spacer()
             Button {
-                
+                if !animationShown {
+                    withAnimation(.easeOut(duration: 2)) {
+                        self.fadeIn.toggle()
+                        self.animationShown.toggle()
+                    }
+                } else {
+                    withAnimation(.easeOut(duration: 2)) {
+                        self.fadeIn = false
+                        self.animationShown.toggle()
+                    }
+                }
             } label: {
-                Text("Fade In")
+                Text(animationShown ? "Fade Out" : "Fade In")
                     .font(.system(size: 16))
                     .fontWeight(.semibold)
                     .foregroundColor(Color(0xFFFFFF))
