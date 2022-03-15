e0d8a079e8ac9d9c6ca00656e56bc923259aded0
swiftJules <juliana.connors.dev@gmail.com>
2022-03-15 12:45:53 -0400

    add button, image and layout animation view

--- Rapptr iOS Test/Views/AnimationScreen.swift
+++ Rapptr iOS Test/Views/AnimationScreen.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 struct AnimationScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // display animation
+            Spacer()
+            Image("ic_logo")
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("Fade In")
+                    .font(.system(size: 16))
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(0xFFFFFF))
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color(0x0E5C89))
+            .padding([.leading, .trailing, .bottom], 10)
+        }
+        .background(Color(0xF9F9F9))
     }
 }
 
 struct AnimationScreen_Previews: PreviewProvider {
     static var previews: some View {
         AnimationScreen()
+    }
+}
+
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue: Double(hex & 0xFF) / 255,
+            opacity: alpha
+        )
     }
 }
