daa736f1fa5c393efaa9ffeccb9d1e4c98d4bc5a
swiftJules <juliana.connors.dev@gmail.com>
2022-03-18 11:08:54 -0400

    add constants for project colors and string literals

--- Rapptr iOS Test/Constants.swift
+++ Rapptr iOS Test/Constants.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import UIKit
+import SwiftUI
+
+struct NetworkConstant {
+    static let chatLog = "http://dev.rapptrlabs.com/Tests/scripts/chat_log.php"
+    static let loginURL = "http://dev.rapptrlabs.com/Tests/scripts/login.php"
+}
+
+struct ChatConstant {
+    static let chatCell = "ChatTableViewCell"
+}
+
+struct ColorConstant {
+    static let viewBackground = UIColor(hex: 0xF9F9F9)
+    static let backgroundBlue = UIColor(hex: 0x0E5C89)
+    static let chatText = UIColor(hex: 0x1B1E1F)
+    static let placeholder = UIColor(hex: 0x5F6063)
+    static let buttonText = UIColor(hex: 0xFFFFFF)
+    static let chatBorder = UIColor(hex: 0xEFEFEF)
+    static let animationScreenBackground = Color(0xF9F9F9)
+    static let animationScreenBlue = Color(0x0E5C89)
+    static let animationScreenText = Color(0xFFFFFF)
+}
