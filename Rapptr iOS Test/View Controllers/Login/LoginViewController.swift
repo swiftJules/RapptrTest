5332c574b35af91ab0dfc76605334bc8c9f40f9f
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-01 14:28:56 -0400

    move text field extension into separate file

--- Rapptr iOS Test/View Controllers/Login/LoginViewController.swift
+++ Rapptr iOS Test/View Controllers/Login/LoginViewController.swift
@@ -124,10 +124,3 @@ extension LoginViewController: UITextViewDelegate {
     }
 }
 
-extension UITextField {
-    func setLeftPaddingPoints(_ amount:CGFloat){
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
-        self.leftView = paddingView
-        self.leftViewMode = .always
-    }
-}
