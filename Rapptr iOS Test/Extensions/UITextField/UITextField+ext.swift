44d7da6f8f9eb29fcf75f8449285bc83ccf21434
Juliana Connors <juliana.connors.dev@gmail.com>
2022-04-01 14:26:43 -0400

    move text field extension into file

---
+++ Rapptr iOS Test/Extensions/UITextField/UITextField+ext.swift
@@ -0,0 +1,16 @@
+//
+//  UITextField+ext.swift
+//  Rapptr iOS Test
+//
+//  Created by RBS70 on 4/1/22.
+//
+
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
